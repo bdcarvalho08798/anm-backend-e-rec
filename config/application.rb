d6672c603a048e05ea70abcddcbe01a620775411
bdcarvalho <bdcarvalho0798@gmail.com>
2024-12-15 23:28:28 +0900

    add changes

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module DocSys
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3001', 'http://localhost:4000', 'http://192.168.72.129:4000', 'http://192.168.72.129' # Frontend origin
+        origins 'http://localhost:3001',, 'http://192.168.4.128:4000', 'http://localhost:4000', 'http://192.168.72.129:4000', 'http://192.168.72.129' # Frontend origin
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
--- config/cors.rb
+++ config/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001', 'http://localhost:4000', 'http://192.168.72.129:4000', 'http://192.168.72.129' # Frontend origin
+      origins 'http://localhost:3001', 'http://localhost:4000', 'http://192.168.72.129:4000', 'http://192.168.72.129', 'http://192.168.4.128:4000' # Frontend origin
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
